927c888df00de4e5e7a6b11d12f7c623fad8eae8
dusannosovic <39407532+dusannosovic@users.noreply.github.com>
2021-08-10 12:36:03 +0200

    Some changes in transaction function

--- main.go
+++ main.go
@@ -17,39 +17,6 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func GetBlocks(client ethclient.Client) []Block {
-	header, _ := client.HeaderByNumber(context.Background(), nil)
-	blockNumber := big.NewInt(header.Number.Int64())
-	blockList := []Block{}
-	var i int64
-	for i = 0; i <= blockNumber.Int64(); i++ {
-
-		block, err := client.BlockByNumber(context.Background(), big.NewInt(i))
-		if err != nil {
-			log.Fatal(err)
-		}
-		_block := &Block{
-			BlockNumber:       block.Number().Int64(),
-			Timestamp:         block.Time(),
-			Difficulty:        block.Difficulty().Uint64(),
-			Hash:              block.Hash().String(),
-			TransactionsCount: len(block.Transactions()),
-			Transactions:      []Transaction{},
-		}
-		for _, tx := range block.Transactions() {
-			_block.Transactions = append(_block.Transactions, Transaction{
-				Hash:     tx.Hash().String(),
-				Value:    tx.Value().String(),
-				Gas:      tx.Gas(),
-				GasPrice: tx.GasPrice().Uint64(),
-				Nonce:    tx.Nonce(),
-				To:       tx.To().String(),
-			})
-		}
-		blockList = append(blockList, *_block)
-	}
-	return blockList
-}
 func GetBlockByNumber(client ethclient.Client, number int64) *Block {
 	//header, _ := client.HeaderByNumber(context.Background(), nil)
 	//blockNumber := big.NewInt(header.Number.Int64())
@@ -106,29 +73,6 @@ func GetLatestBlock(client ethclient.Client) *Block {
 	}
 	return _block
 }
-func printBlocks(blk []Block) {
-	i := 0
-	for _, bl := range blk {
-		fmt.Println("------------------------------------Blok-----------------------------------", i)
-		fmt.Println("Blocknumber: ", bl.BlockNumber)
-		fmt.Println("Timestamp: ", bl.Timestamp)
-		fmt.Println("Difficulty: ", bl.Difficulty)
-		fmt.Println("Hash :", bl.Hash)
-		fmt.Println("Transactions count: ", bl.TransactionsCount)
-		fmt.Println("Transactions of Block[", i, "]")
-		for _, tx := range bl.Transactions {
-			fmt.Println("       Hash: ", tx.Hash)
-			fmt.Println("       Value: ", tx.Value)
-			fmt.Println("       s: ", tx.Gas)
-			fmt.Println("       Gas price: ", tx.GasPrice)
-			fmt.Println("       Nonce: ", tx.Nonce)
-			fmt.Println("       To: ", tx.To)
-			fmt.Println("       Pending: ", tx.Pending)
-		}
-		i++
-		fmt.Println()
-	}
-}
 func printLatestBlock(bl Block) {
 	fmt.Println("---------------------------------Blok---------------------------------")
 	fmt.Println("Blocknumber: ", bl.BlockNumber)
@@ -150,18 +94,18 @@ func printLatestBlock(bl Block) {
 	}
 }
 
-func Transfer(client ethclient.Client, privKey string, to string, amount int64) (string, error) {
+func transactionF(client ethclient.Client, privKey string, to string, amount int64) (string, error) {
 	defer func() {
 		if err := recover(); err != nil {
 			fmt.Println(err)
 		}
 	}()
-	privateKey, err := crypto.HexToECDSA(privKey)
+	privateKeyBytes, err := crypto.HexToECDSA(privKey)
 	if err != nil {
 		return "", err
 	}
 
-	publicKey := privateKey.Public()
+	publicKey := privateKeyBytes.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
 	if !ok {
 		return "", err
@@ -193,7 +137,7 @@ func Transfer(client ethclient.Client, privKey string, to string, amount int64) 
 		return "", err
 	}
 
-	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKeyBytes)
 
 	if err != nil {
 		return "", err
@@ -206,52 +150,60 @@ func Transfer(client ethclient.Client, privKey string, to string, amount int64) 
 
 	return signedTx.Hash().String(), nil
 }
-func GetAddressBalance(client ethclient.Client, address string) (string, error) {
-	account := common.HexToAddress(address)
-	balance, err := client.BalanceAt(context.Background(), account, nil)
+func GetAddressBalanceFromPrivKey(client ethclient.Client, privKey string) (string, error) {
+	privateKeyBytes, err := crypto.HexToECDSA(privKey)
 	if err != nil {
-		return "0", err
+		return "", err
 	}
 
-	return balance.String(), nil
+	publicKey := privateKeyBytes.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		return "", err
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	balance, err := client.BalanceAt(context.Background(), fromAddress, nil)
+	return balance.String(), err
 }
 
 func getInput(prompt string, r *bufio.Reader) (string, error) {
 	fmt.Println(prompt)
 	input, err := r.ReadString('\n')
-
 	return strings.TrimSpace(input), err
 }
 
 func promptOptions(client ethclient.Client) {
 	reader := bufio.NewReader(os.Stdin)
 
-	opt, _ := getInput("Chose option(1 - Read last block, 2 - Make transfer, 3 - Account Balance, 4 Read block by number)", reader)
+	opt, _ := getInput("Chose option(1 - Read last block, 2 - Make transfer, 3 - Read block by number)", reader)
 
 	switch opt {
 	case "1":
 		printLatestBlock(*GetLatestBlock(client))
 		promptOptions(client)
 	case "2":
-		fmt.Println("Transaction Hash:")
 		privKey, _ := getInput("Insert private key: ", reader)
 		toAddress, _ := getInput("Insert hex address To", reader)
-		amount, _ := getInput("Insert amount", reader)
-		a, err := strconv.ParseInt(amount, 10, 64)
-		if err != nil {
-			fmt.Println("The price must be a number")
-			promptOptions(client)
-		}
-		fmt.Println(Transfer(client, privKey, toAddress, a))
-	case "3":
-		acccountAddress, _ := getInput("Insert hex account address", reader)
-		ammount, err := GetAddressBalance(client, acccountAddress)
+		ammount, err := GetAddressBalanceFromPrivKey(client, privKey)
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Print("Account ammount:  ", ammount, "\n")
+		tString := fmt.Sprintln("Insert amount from 0 to ", ammount)
+		amount, _ := getInput(tString, reader)
+		a, err := strconv.ParseInt(amount, 10, 64)
+		if err != nil {
+			fmt.Println("The ammount must be a number")
+			promptOptions(client)
+		}
+		transactioHash, err := transactionF(client, privKey, toAddress, a)
+		if err != nil {
+			fmt.Println(err)
+			promptOptions(client)
+		}
+		fmt.Println("Transaction Hash :", transactioHash)
 		promptOptions(client)
-	case "4":
+	case "3":
 		header, _ := client.HeaderByNumber(context.Background(), nil)
 		blockNumber := header.Number.Int64()
 		fmt.Println("Choose one number between 0 and ", blockNumber)
